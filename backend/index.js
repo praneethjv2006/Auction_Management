605b2052c57354b474a15f7b437845efbdc51b8a
J V Praneeth <2023mcb1296@iitrpr.ac.in>
2026-03-13 14:26:32 +0530

    frontend deploy

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const { ensureDefaultOrganizer } = require('./controllers/organizerController');
 
 const app = express();
 const server = http.createServer(app);
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
